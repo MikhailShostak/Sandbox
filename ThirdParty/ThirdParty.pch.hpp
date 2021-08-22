b3ba724d0f9fb99e3f82ae6728a7566232c56c93
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2021-08-23 00:11:36 +0300

    Add ThirdParty precompiled header

---
+++ ThirdParty/ThirdParty.pch.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <new>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <type_traits>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include <boost/process.hpp>
+
+#ifndef IMGUI_DEFINE_MATH_OPERATORS
+#define IMGUI_DEFINE_MATH_OPERATORS
+#endif
+#include <imgui.h>
+#include <imgui_stdlib.h>
+#include <imgui_internal.h>
+
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <fmt/chrono.h>
+#include <fmt/ostream.h>
+
+#include <range/v3/all.hpp>
+
+#include <yaml-cpp/yaml.h>
