5cf46897a03f9cdb4e6d7ccc23c4ccea9a90cbdf
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2021-10-18 22:26:40 +0300

    Move declarations to a separate folder

--- Frameworks/ClassGen/ClassGen.hpp
+++ Frameworks/ClassGen/ClassGen.hpp
@@ -3,14 +3,14 @@
 #include <Utils.hpp>
 
 //TODO: sort
-#include "Classes/BaseInfo.hpp"
-#include "Classes/FileInfo.hpp"
-#include "Classes/TypeInfo.hpp"
-#include "Classes/TypeInfoSerializer.hpp"
-#include "Classes/ParameterInfo.hpp"
-#include "Classes/FunctionInfo.hpp"
-#include "Classes/PropertyInfo.hpp"
+#include "Classes/Declaration/BaseInfo.hpp"
+#include "Classes/Declaration/FileInfo.hpp"
+#include "Classes/Declaration/TypeInfo.hpp"
+#include "Classes/Declaration/TypeInfoSerializer.hpp"
+#include "Classes/Declaration/ParameterInfo.hpp"
+#include "Classes/Declaration/FunctionInfo.hpp"
+#include "Classes/Declaration/PropertyInfo.hpp"
 #include "Classes/Graph/ConnectionInfo.hpp"
 #include "Classes/Graph/NodeInfo.hpp"
 #include "Classes/Graph/GraphInfo.hpp"
-#include "Classes/ClassInfo.hpp"
+#include "Classes/Declaration/ClassInfo.hpp"
