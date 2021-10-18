10f27259b3094028ba7e6250ac0cfa4f39b24884
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2021-10-18 21:56:27 +0300

    Fix set and map aliases

--- Frameworks/Utils/Containers/Map.hpp
+++ Frameworks/Utils/Containers/Map.hpp
@@ -2,5 +2,5 @@
 
 #include <unordered_map>
 
-template<typename Key, typename Type, typename Comparator = std::less<Key>, typename Allocator = std::allocator<std::pair<const Key, Type>>>
-using Map = std::unordered_map<Key, Type, Comparator, Allocator>;
+template<typename Key, typename Type, typename KeyHasher = std::hash<Key>, typename KeyComparator = std::equal_to<Key>, typename Allocator = std::allocator<std::pair<const Key, Type>>>
+using Map = std::unordered_map<Key, Type, KeyHasher, KeyComparator, Allocator>;
--- Frameworks/Utils/Containers/Set.hpp
+++ Frameworks/Utils/Containers/Set.hpp
@@ -2,5 +2,5 @@
 
 #include <unordered_set>
 
-template<typename Key, typename Comparator = std::less<Key>, typename Allocator = std::allocator<Key>>
-using Set = std::unordered_set<Key, Comparator, Allocator>;
+template<typename Key, typename Hasher = std::hash<Key>, typename Comparator = std::equal_to<Key>, typename Allocator = std::allocator<Key>>
+using Set = std::unordered_set<Key, Hasher, Comparator, Allocator>;
