32e752a5901f3146fe9c961937c904268cfbb35c
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2021-12-11 05:41:55 +0300

    Add debug function to check template types over compilation output

--- Frameworks/Utils/Debug/DebugUtils.hpp
+++ Frameworks/Utils/Debug/DebugUtils.hpp
@@ -14,4 +14,16 @@ inline void ExecuteOnce(Lambda &&lambda)
     }
 }
 
+template<typename Type, typename ...Types>
+void CheckTypesInternally()
+{
+    static_assert(sizeof(Type) < 0);
+}
+
+template<typename ...Types>
+void CheckTypes()
+{
+    CheckTypesInternally<Types...>();
+}
+
 }
