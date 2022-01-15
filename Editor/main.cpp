69151da93d188f2d5dcae5d8c37f42c26376302a
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2022-01-15 07:27:24 +0300

    Add generic ClassGen extensions loading

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -8,8 +8,6 @@
 
 #include <Class.gen.hpp>
 
-Array<UniqueReference<ClassGen::ExtensionInterface>> g_Extensions;
-
 void OnContentUpdate()
 {
     ImGuiID Workspace = ImGui::GetID("Workspace");
@@ -110,13 +108,7 @@ public:
         Editors.insert({"TextEditor", std::make_unique<DefaultExtensions::TextEditor>() });
         Editors.insert({"ClassGenEditor", std::make_unique<DefaultExtensions::ClassGenEditor>() });
         
-        auto extensions = Reflection::FindAll<ClassGen::ExtensionInterface>();
-        for (auto &e : extensions)
-        {
-            auto i = e->Create();
-            i->Load(g_ExtensionLibrary);
-            g_Extensions.push_back(std::move(i));
-        }
+        ClassGen::LoadExtensions();
 
         ReloadFiles();
 
--- Frameworks/ClassGen/ClassGenGlobals.hpp
+++ Frameworks/ClassGen/ClassGenGlobals.hpp
@@ -8,3 +8,21 @@ inline Map<String, ClassGen::FunctionInfo> g_FunctionCache;
 inline SortedMap<String, ClassGen::FileInfo> g_ClassGenCache;
 
 inline Map<String, UniqueReference<ClassGen::FileEditor>> g_Editors;
+
+inline Array<UniqueReference<ClassGen::ExtensionInterface>> g_Extensions;
+
+namespace ClassGen
+{
+
+inline void LoadExtensions()
+{
+    auto extensions = Reflection::FindAll<ClassGen::ExtensionInterface>();
+    for (auto& e : extensions)
+    {
+        auto i = e->Create();
+        i->Load(g_ExtensionLibrary);
+        g_Extensions.push_back(std::move(i));
+    }
+}
+
+}
