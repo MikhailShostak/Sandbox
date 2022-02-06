92790888c9bb17ad981c7d0cc71810a9dc636f61
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2022-02-06 15:08:10 +0300

    Add separate function to load textures into already created references

--- Frameworks/Graphics/Texture/TextureLoader.cpp
+++ Frameworks/Graphics/Texture/TextureLoader.cpp
@@ -2,40 +2,42 @@
 
 namespace Graphics
 {
-
-SharedReference<Graphics::Texture> LoadTexture(const System::Path &path)
+void LoadTexture(Graphics::Texture& texture, const System::Path& path)
 {
-    SharedReference<Graphics::Texture> texture = CreateShared<Graphics::Texture>();
-
     int width, height, channels;
-    unsigned char *data = stbi_load(path.string().data(), &width, &height, &channels, 0);
+    unsigned char* data = stbi_load(path.string().data(), &width, &height, &channels, 0);
 
-    texture->Size.x = width;
-    texture->Size.y = height;
-    texture->Channels = channels;
-    if (texture->Channels == 3)
+    texture.Size.x = width;
+    texture.Size.y = height;
+    texture.Channels = channels;
+    if (texture.Channels == 3)
     {
-        texture->Channels = 4;
+        texture.Channels = 4;
         size_t originalSize = width * height * channels;
-        size_t newSize = width * height * texture->Channels;
-        texture->Buffer.resize(newSize);
+        size_t newSize = width * height * texture.Channels;
+        texture.Buffer.resize(newSize);
         size_t i = 0;
         size_t j = 0;
         while (i < originalSize)
         {
-            texture->Buffer[j] = data[i];
-            texture->Buffer[j + 1] = data[i + 1];
-            texture->Buffer[j + 2] = data[i + 2];
-            texture->Buffer[j + 3] = std::numeric_limits<std::remove_reference_t<decltype(texture->Buffer[j + 3])>>::max();
+            texture.Buffer[j] = data[i];
+            texture.Buffer[j + 1] = data[i + 1];
+            texture.Buffer[j + 2] = data[i + 2];
+            texture.Buffer[j + 3] = std::numeric_limits<std::remove_reference_t<decltype(texture.Buffer[j + 3])>>::max();
             i += channels;
-            j += texture->Channels;
+            j += texture.Channels;
         }
     }
     else
     {
-        texture->Buffer = Array<uint8_t>(data, data + width * height * channels);
+        texture.Buffer = Array<uint8_t>(data, data + width * height * channels);
     }
+}
 
+SharedReference<Graphics::Texture> LoadTexture(const System::Path &path)
+{
+    SharedReference<Graphics::Texture> texture = CreateShared<Graphics::Texture>();
+    LoadTexture(*texture, path);
     return texture;
 }
 
