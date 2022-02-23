b06932e2b27a5aaa02f1dff68a3be6cebeef7941
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2022-02-23 08:10:41 +0300

    Add return value perfect forwarding to all casts

--- Frameworks/Utils/Memory/Casts.hpp
+++ Frameworks/Utils/Memory/Casts.hpp
@@ -5,171 +5,171 @@
 #include "Memory/References.hpp"
 
 template<typename Type>
-inline constexpr Type &ConstCast(const Type &value)
+inline constexpr decltype(auto) ConstCast(const Type &value)
 {
     return const_cast<Type &>(value);
 }
 
 template<typename Type>
-inline constexpr Type &&ConstCast(const Type &&value)
+inline constexpr decltype(auto) ConstCast(const Type &&value)
 {
     return const_cast<Type &&>(value);
 }
 
 template<typename Type>
-inline constexpr Type *ConstCast(const Type *value)
+inline constexpr decltype(auto) ConstCast(const Type *value)
 {
     return const_cast<Type *>(value);
 }
 
 template<typename To, typename From>
-inline constexpr To &ReinterpretCast(From &from)
+inline constexpr decltype(auto) ReinterpretCast(From &from)
 {
     return reinterpret_cast<To &>(from);
 }
 
 template<typename To, typename From>
-inline constexpr To &&ReinterpretCast(From &&from)
+inline constexpr decltype(auto) ReinterpretCast(From &&from)
 {
     return reinterpret_cast<To &&>(from);
 }
 
 template<typename To, typename From>
-inline constexpr To *ReinterpretCast(From *from)
+inline constexpr decltype(auto) ReinterpretCast(From *from)
 {
     return reinterpret_cast<To *>(from);
 }
 
 template<typename To, typename From>
-inline constexpr auto DynamicCast(From &&from) -> Meta::CopyModifiers<Meta::RemoveExtraPointerReference<decltype(from)>, To>
+inline constexpr decltype(auto) DynamicCast(From &&from)
 {
     using ResultType = Meta::CopyModifiers<Meta::RemoveExtraPointerReference<decltype(from)>, To>;
     return dynamic_cast<ResultType>(std::forward<From>(from));
 }
 
 template<typename To, typename From>
-inline constexpr auto StaticCast(From &&from) -> Meta::CopyModifiers<Meta::RemoveExtraPointerReference<decltype(from)>, To>
+inline constexpr decltype(auto) StaticCast(From &&from)
 {
     using ResultType = Meta::CopyModifiers<Meta::RemoveExtraPointerReference<decltype(from)>, To>;
     return static_cast<ResultType>(std::forward<From>(from));
 }
 
 template<typename To, typename From>
-inline constexpr auto ConstCast(const SharedReference<From> &from)
+inline constexpr decltype(auto) ConstCast(const SharedReference<From> &from)
 {
     return std::const_pointer_cast<To>(from);
 }
 
 template<typename To, typename From>
-inline constexpr auto ConstCast(SharedReference<From> &from)
+inline constexpr decltype(auto) ConstCast(SharedReference<From> &from)
 {
     return std::const_pointer_cast<To>(from);
 }
 
 template<typename To, typename From>
-inline constexpr auto ConstCast(SharedReference<From> &&from)
+inline constexpr decltype(auto) ConstCast(SharedReference<From> &&from)
 {
     return std::const_pointer_cast<To>(std::move(from));
 }
 
 template<typename To, typename From>
-inline constexpr auto ReinterpretCast(const SharedReference<From> &from)
+inline constexpr decltype(auto) ReinterpretCast(const SharedReference<From> &from)
 {
     return boost::reinterpret_pointer_cast<To>(from); //TODO: fix macOS
 }
 
 template<typename To, typename From>
-inline constexpr auto ReinterpretCast(SharedReference<From> &from)
+inline constexpr decltype(auto) ReinterpretCast(SharedReference<From> &from)
 {
     return boost::reinterpret_pointer_cast<To>(from); //TODO: fix macOS
 }
 
 template<typename To, typename From>
-inline constexpr auto ReinterpretCast(SharedReference<From> &&from)
+inline constexpr decltype(auto) ReinterpretCast(SharedReference<From> &&from)
 {
     return boost::reinterpret_pointer_cast<To>(std::move(from)); //TODO: fix macOS
 }
 
 template<typename To, typename From>
-inline constexpr auto DynamicCast(const SharedReference<From> &from)
+inline constexpr decltype(auto) DynamicCast(const SharedReference<From> &from)
 {
     return std::dynamic_pointer_cast<To>(from);
 }
 
 template<typename To, typename From>
-inline constexpr auto DynamicCast(SharedReference<From> &from)
+inline constexpr decltype(auto) DynamicCast(SharedReference<From> &from)
 {
     return std::dynamic_pointer_cast<To>(from);
 }
 
 template<typename To, typename From>
-inline constexpr auto DynamicCast(SharedReference<From> &&from)
+inline constexpr decltype(auto) DynamicCast(SharedReference<From> &&from)
 {
     return std::dynamic_pointer_cast<To>(std::move(from));
 }
 
 template<typename To, typename From>
-inline constexpr auto StaticCast(const SharedReference<From> &from)
+inline constexpr decltype(auto) StaticCast(const SharedReference<From> &from)
 {
     return std::static_pointer_cast<To>(from);
 }
 
 template<typename To, typename From>
-inline constexpr auto StaticCast(SharedReference<From> &from)
+inline constexpr decltype(auto) StaticCast(SharedReference<From> &from)
 {
     return std::static_pointer_cast<To>(from);
 }
 
 template<typename To, typename From>
-inline constexpr auto StaticCast(SharedReference<From> &&from)
+inline constexpr decltype(auto) StaticCast(SharedReference<From> &&from)
 {
     return std::static_pointer_cast<To>(std::move(from));
 }
 
 template<typename To, typename From>
-inline constexpr auto ConstCast(UniqueReference<From> &from)
+inline constexpr decltype(auto) ConstCast(UniqueReference<From> &from)
 {
     return UniqueReference<To>{ ConstCast<To>(from.release()) };
 }
 
 template<typename To, typename From>
-inline constexpr auto ConstCast(UniqueReference<From> &&from)
+inline constexpr decltype(auto) ConstCast(UniqueReference<From> &&from)
 {
     return UniqueReference<To>{ ConstCast<To>(from.release()) };
 }
 
 template<typename To, typename From>
-inline constexpr auto ReinterpretCast(UniqueReference<From> &from)
+inline constexpr decltype(auto) ReinterpretCast(UniqueReference<From> &from)
 {
     return UniqueReference<To>{ ReinterpretCast<To>(from.release()) };
 }
 
 template<typename To, typename From>
-inline constexpr auto ReinterpretCast(UniqueReference<From> &&from)
+inline constexpr decltype(auto) ReinterpretCast(UniqueReference<From> &&from)
 {
     return UniqueReference<To>{ ReinterpretCast<To>(from.release()) };
 }
 
 template<typename To, typename From>
-inline constexpr auto DynamicCast(UniqueReference<From> &from)
+inline constexpr decltype(auto) DynamicCast(UniqueReference<From> &from)
 {
     return UniqueReference<To>{ DynamicCast<To>(from.release()) };
 }
 
 template<typename To, typename From>
-inline constexpr auto DynamicCast(UniqueReference<From> &&from)
+inline constexpr decltype(auto) DynamicCast(UniqueReference<From> &&from)
 {
     return UniqueReference<To>{ DynamicCast<To>(from.release()) };
 }
 
 template<typename To, typename From>
-inline constexpr auto StaticCast(UniqueReference<From> &from)
+inline constexpr decltype(auto) StaticCast(UniqueReference<From> &from)
 {
     return UniqueReference<To>{ StaticCast<To>(from.release()) };
 }
 
 template<typename To, typename From>
-inline constexpr auto StaticCast(UniqueReference<From> &&from)
+inline constexpr decltype(auto) StaticCast(UniqueReference<From> &&from)
 {
     return UniqueReference<To>{ StaticCast<To>(from.release()) };
 }
