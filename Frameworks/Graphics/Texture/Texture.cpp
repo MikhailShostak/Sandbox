9cef86c6b727ec42abafe2a410613c5abda7585b
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2022-02-23 08:25:09 +0300

    Switch textures to use tiling by default

--- Frameworks/Graphics/Texture/Texture.cpp
+++ Frameworks/Graphics/Texture/Texture.cpp
@@ -7,7 +7,7 @@ Texture::Texture():
     Size(hlslpp::int3{0, 0, 0}),
     Channels(0),
     Filtration(Graphics::TextureFiltration::Bilinear),
-    Wrapping(Graphics::TextureWrapping::WrapToBorder)
+    Wrapping(Graphics::TextureWrapping::TiledWrapping)
 {
 
 }
