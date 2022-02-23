98eee43a3a2c91a4dc8023eb27693a5cc2217ec8
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2022-02-23 08:23:29 +0300

    Expose loading of texture data for existed texture instance

--- Frameworks/Graphics/Texture/TextureLoader.hpp
+++ Frameworks/Graphics/Texture/TextureLoader.hpp
@@ -6,6 +6,7 @@ namespace Graphics
 SharedReference<Graphics::Texture> CreateTextureBuffer(const Int3& size);
 SharedReference<Graphics::Texture> CreateTexture(const Color& color, const Int3& size = { 1, 1, 1 });
 
+void LoadTexture(Graphics::Texture& texture, const System::Path& path);
 SharedReference<Graphics::Texture> LoadTexture(const System::Path &path);
 void SaveTexture(const SharedReference<Graphics::Texture> &texture, const System::Path &path);
 
